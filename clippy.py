18224ac8cf33522efe6fe82ea6161e7ea8cc1575
clstatham <58794204+clstatham@users.noreply.github.com>
2025-07-11 14:56:31 -0500

    fix clippy.py for windows

--- clippy.py
+++ clippy.py
@@ -11,9 +11,20 @@ if __name__ == "__main__":
     workspace_root = pathlib.Path(meta["workspace_root"])
     members = [pkg for pkg in meta["packages"] if pkg["id"] in meta["workspace_members"]]
 
+    # check if we're on windows, linux, or macOS
+    if sys.platform.startswith("win"):
+        system_arch = "x86_64-pc-windows-msvc"
+    elif sys.platform.startswith("linux"):
+        system_arch = "x86_64-unknown-linux-gnu"
+    elif sys.platform.startswith("darwin"):
+        system_arch = "x86_64-apple-darwin"
+    else:
+        print(f"Unsupported platform: {sys.platform}")
+        sys.exit(1)
+
     TARGETS = {
-        "builder": "x86_64-unknown-linux-gnu",
-        "loader": "x86_64-unknown-linux-gnu",
+        "builder": system_arch,
+        "loader": system_arch,
         "kernel": "aarch64-unknown-none",
         "bootloader": "aarch64-unknown-none",
         "chainloader": "aarch64-unknown-none",
